3a3496734693e753319bad720ca704a20c3f1406
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-04-19 01:29:43 +0000

    main

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Created by: Aiden McLeod
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area of a circle.
+ */
+function calculate () {
+  // input
+  const length = parseFloat(document.getElementById('length').value)
+  const width = parseFloat(document.getElementById('width').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+  // process
+  
+  const area = ( length * width * height ) / 3
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area.toFixed(2) + ' cm²'
+}
